2cdedb3ab1c601eeb376beb6afdac5c3d903c1bf
Alejandro Salas <asalas.wd@outlook.com>
2022-11-14 15:12:54 -0500

    Generate base constructor and create TabbedPane instance

--- src/com/java21days/MainPanel.java
+++ src/com/java21days/MainPanel.java
@@ -2,7 +2,7 @@ package com.java21days;
 
 public class MainPanel {
     public static void main(String[] args) {
-        System.out.println("This is my main method");
+        TabbedPane tabbedPane = new TabbedPane("Tabbed Panes");
     }
 
 }
--- src/com/java21days/TabbedPane.java
+++ src/com/java21days/TabbedPane.java
@@ -1,7 +1,17 @@
 package com.java21days;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TabbedPane extends JFrame {
+    Dimension dim = new Dimension(500, 200);
 
+    TabbedPane(String title){
+        super(title);
+        setSize(dim);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 }
